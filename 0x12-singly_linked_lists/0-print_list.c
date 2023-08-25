96871d8f80055c78e48c80fb545bd89bfaafa56c
abdelrahman mohmed <119086487+abdelrahmanmohm@users.noreply.github.com>
2023-08-25 20:22:11 +0000

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t l = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %l\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		l++;
+		s++;
 	}
 
 	return (l);
