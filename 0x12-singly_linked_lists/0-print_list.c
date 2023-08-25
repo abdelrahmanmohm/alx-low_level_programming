439462e206dc2861825990333349983df14810aa
abdelrahman mohmed <119086487+abdelrahmanmohm@users.noreply.github.com>
2023-08-25 20:24:54 +0000

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,9 @@
 #include "lists.h"
 
 /**
- * print_list -> print all the elements of a linked list
- * @h: pointer to the list_t-> list to print
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
@@ -20,5 +21,5 @@ size_t print_list(const list_t *h)
 		s++;
 	}
 
-	return (l);
+	return (s);
 }
